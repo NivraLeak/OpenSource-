138188bbdf22e5b7f27136d3c4b81732c362ce47
nivra <nivraleak@outlook.com>
2020-06-05 18:16:26 -0500

    Nueva rama

--- ideogo/src/main/java/com/acme/ideogo/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.ideogo.repository;
-
-import com.acme.ideogo.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
