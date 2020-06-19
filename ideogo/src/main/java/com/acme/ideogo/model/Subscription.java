60507976ff50109a9b4bb1b5c23e584bfce0497c
William <wmavalle@gmail.com>
2020-06-18 19:07:25 -0500

    TB04

--- ideogo/src/main/java/com/acme/ideogo/model/Subscription.java
+++ ideogo/src/main/java/com/acme/ideogo/model/Subscription.java
@@ -1,4 +1,29 @@
 package com.acme.ideogo.model;
 
-public class Subscription {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "subscription")
+@Getter
+@Setter
+public class Subscription extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 30)
+    @Column(unique = true)
+    private String name;
+    @NotNull
+    @NotBlank
+    private float price;
+
 }
