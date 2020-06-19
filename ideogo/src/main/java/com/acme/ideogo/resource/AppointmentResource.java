8046840ef91948279c9d472e779b11ad340420ac
William <wmavalle@gmail.com>
2020-06-19 12:00:17 -0500

    6.0

--- ideogo/src/main/java/com/acme/ideogo/resource/AppointmentResource.java
+++ ideogo/src/main/java/com/acme/ideogo/resource/AppointmentResource.java
@@ -1,4 +1,13 @@
 package com.acme.ideogo.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
 public class AppointmentResource {
+
+    public Date Date_time ;
+
 }
