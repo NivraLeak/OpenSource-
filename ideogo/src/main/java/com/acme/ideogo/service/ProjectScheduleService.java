7bec97aded7bc00376e90e59fdd3d8797b137f5a
nivra <nivraleak@outlook.com>
2020-06-19 16:49:21 -0500

    Properties

--- ideogo/src/main/java/com/acme/ideogo/service/ProjectScheduleService.java
+++ ideogo/src/main/java/com/acme/ideogo/service/ProjectScheduleService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+
 public interface ProjectScheduleService {
     ProjectSchedule getProjectScheduleById(Long projectScheduleId);
     ProjectSchedule getProjectScheduleByIdAndProjectId(Long projectId, Long projectScheduleId);
--- ideogo/src/main/java/com/acme/ideogo/service/ProjectScheduleServiceImpl.java
+++ ideogo/src/main/java/com/acme/ideogo/service/ProjectScheduleServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ProjectScheduleServiceImpl implements ProjectScheduleService{
 
