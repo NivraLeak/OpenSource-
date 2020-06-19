6c8a3f626f26b91679ed529fb7f996149e4f71ca
William <wmavalle@gmail.com>
2020-06-19 12:18:02 -0500

    TB04 2.0

--- ideogo/src/main/java/com/acme/ideogo/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.ideogo.repository;
-
-import com.acme.ideogo.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
